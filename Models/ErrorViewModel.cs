09ee0e6ffd9523c91dd13433b0779c7ee04dd1fe
Nuri TIRAŞ <nuritiras@gmail.com>
2025-02-12 20:18:37 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BilisimAkademi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
