fb5acaf0f80f97fda5a3ccd6ebaaa1e17418b73e
Nuri TIRAŞ <nuritiras@gmail.com>
2025-02-16 18:19:33 +0300

    kursiyer kayit

---
+++ Controllers/KursController.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BilisimAkademi.Models;
+
+namespace BilisimAkademi.Controllers
+{
+    public class KursController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+    }
+}
